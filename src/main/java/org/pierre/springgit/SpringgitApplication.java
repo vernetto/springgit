0f494d4faf685fed95e9ddd3eceb2fd13e1630ce
vernetto <publicpierre@gmail.com>
2019-06-24 20:35:24 +0200

    working

--- src/main/java/org/pierre/springgit/SpringgitApplication.java
+++ src/main/java/org/pierre/springgit/SpringgitApplication.java
@@ -2,12 +2,26 @@ package org.pierre.springgit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class SpringgitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringgitApplication.class, args);
+    }
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(false);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
     }
 
 }
