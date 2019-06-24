7a91e4a8f7c029bd45c8cea6d7f75a88d59d0e73
vernetto <publicpierre@gmail.com>
2019-06-24 19:57:32 +0200

    first

---
+++ src/main/java/org/pierre/springgit/SpringgitApplication.java
@@ -0,0 +1,13 @@
+package org.pierre.springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringgitApplication.class, args);
+    }
+
+}
